67bdb3fae2c05d79efe05e1a749cb4eb85e6ca34
luisdiego18 <diegotapia1987@gmail.com>
2022-12-26 17:29:23 -0600

    Initial commit

---
+++ src/main/java/com/example/awsdemo1/HomeController.java
@@ -0,0 +1,13 @@
+package com.example.awsdemo1;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/api/home")
+    public String home(){
+        return "Home";
+    }
+}
